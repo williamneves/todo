4217a24838357b3fa59769fa796147307a90c015
gwilliamnn <97652695+gwilliamnn@users.noreply.github.com>
2022-06-09 15:47:24 -0400

    More

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,34 +1,35 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom';
-import {auth} from "../lib/firebase";
-import { useAuthState } from 'react-firebase-hooks/auth';
-import {BallTriangle} from "react-loader-spinner";
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { auth } from "../lib/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { BallTriangle } from "react-loader-spinner";
 
-const ProtectedRoute = ({children}) => {
+const ProtectedRoute = ({ children }) => {
   const [user, loading, error] = useAuthState(auth);
-
-  // Set the user in local storage
-
 
   // If fakeAuth is true, redirect to login page
   if (loading) {
     return (
-			<div
-				className={
-					'h-screen w-screen flex flex-col gap-5 justify-center items-center align-middle bg-gray-300-100'
-				}>
-				<BallTriangle height='150' width='150' color='orange' ariaLabel='loading-indicator' />
-				<code className={'text-xl text-gray-600 italic '}>Loading...</code>
-			</div>
-		);
+      <div
+        className={
+          "h-screen w-screen flex flex-col gap-5 justify-center items-center align-middle bg-gray-300-100"
+        }
+      >
+        <BallTriangle
+          height="150"
+          width="150"
+          color="orange"
+          ariaLabel="loading-indicator"
+        />
+        <code className={"text-xl text-gray-600 italic "}>Loading...</code>
+      </div>
+    );
   }
 
   // Is loading and no User founded
   if (!loading && !user) {
-    return <Navigate to='/login' replace/>
+    return <Navigate to="/login" replace />;
   } else return children;
+};
 
-}
-
-
-export default ProtectedRoute
+export default ProtectedRoute;
