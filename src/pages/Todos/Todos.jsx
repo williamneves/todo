f25a8ed82c17cdd7179c6d8ee05f2b2693690651
gwilliamnn <97652695+gwilliamnn@users.noreply.github.com>
2022-06-15 10:16:10 -0400

    more stuffs

--- src/pages/Todos/Todos.jsx
+++ src/pages/Todos/Todos.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./Todos.css";
+import Datepicker from "flowbite-datepicker/Datepicker";
 
 const Todos = () => {
+  // Handle create new task
+  const handleAddNewTask = (e) => {
+    e.preventDefault();
+    console.log("handleCreateTask");
+  };
   return (
     <div className={"flex justify-center w-full pt-10"}>
       <div className="flex flex-col gap-3 items-center w-full mx-8 md:max-w-xl lg:max-w-2xl">
@@ -26,17 +32,17 @@ const Todos = () => {
             <input
               type="text"
               id="email-address-icon"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-200 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="name@flowbite.com"
             />
           </div>
         </div>
         <div
           className={
-            "flex justify-between items-center h-14 w-full rounded-xl drop-shadow-md"
+            "flex justify-between items-center h-16 w-full rounded-xl drop-shadow-md"
           }
         >
-          <div className="relative h-full w-full">
+          <form className="relative h-full w-full" onSubmit={handleAddNewTask}>
             {/*  Check Css Icon */}
             <div className="absolute inset-y-0 left-0 flex items-center pl-5 cursor-pointer">
               <div className="h-6 w-6 rounded-full border-2 border-gray-400/75 dark:border-gray-200/75 dark:hover:border-gray-200 hover:scale-105 active:scale-95 hover:border-gray-400 transition-all ease-out duration-200"></div>
@@ -44,10 +50,17 @@ const Todos = () => {
             <input
               type="text"
               id="email-address-icon"
-              className="rounded-xl bg-white border-0 dark:bg-secondary-dark-bg/75 text-gray-900 text-sm focus:ring-2 focus:ring-orange-300 focus:border-orange-300 block h-full w-full pl-14 p-2 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
-              placeholder="name@flowbite.com"
+              className="rounded-xl bg-white border-0 dark:bg-secondary-dark-bg/75 text-gray-900 focus:ring-2 focus:ring-gray-300 focus:border-gray-300 block h-full w-full pl-16 p-2 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 transition-all ease-out duration-200 overflow-x-scroll pr-20"
+              placeholder="Add new task here..."
             />
-          </div>
+            <input
+              datepicker
+              datepicker-autohide
+              type="text"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Select date"
+            />
+          </form>
         </div>
       </div>
     </div>
